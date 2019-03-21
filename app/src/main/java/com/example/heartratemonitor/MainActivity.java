262a2d8c6f80b1ac54800e04dfc9a70d85694ea5
Skrimm42 <skrimm2skrimm@gmail.com>
2019-03-21 22:00:31 +0200

    Add Charting functionality

--- app/src/main/java/com/example/heartratemonitor/MainActivity.java
+++ app/src/main/java/com/example/heartratemonitor/MainActivity.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 import com.github.mikephil.charting.charts.LineChart;
 
 public class MainActivity extends AppCompatActivity {
 
-    Charting cardio_chart;
+    Charting ecg_chart, hr_chart;
+    Thread thr; //del
+    boolean run = false;//del
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +27,27 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(R.string.app_name);
 
         LineChart lCh = findViewById(R.id.cardio_chart);
-        cardio_chart = new Charting(lCh);
+        LineChart lCh1 = findViewById(R.id.bpm_chart);
+        ecg_chart = new Charting(lCh, Charting.ChartType.ECG);
+        ecg_chart.setMaxViewPoints(2000);
 
-        cardio_chart.setMaxViewPoints(2000);
+        hr_chart = new Charting(lCh1, Charting.ChartType.HR);//Heart Rate chart
+        hr_chart.setMaxViewPoints(150);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
-        menu.findItem(R.id.menu_run).setVisible(true);
-        menu.findItem(R.id.menu_stop).setVisible(false);
+        if (!run) {
+            menu.findItem(R.id.menu_run).setVisible(true);
+            menu.findItem(R.id.menu_stop).setVisible(false);
+        } else{
+            menu.findItem(R.id.menu_run).setVisible(false);
+            menu.findItem(R.id.menu_stop).setVisible(true);
+        }
+
+
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -46,7 +60,51 @@ public class MainActivity extends AppCompatActivity {
             case R.id.menu_settings:
                 startActivity(new Intent(this, SettingsActivity.class));
                 return true;
+            case R.id.menu_run:
+                run = true;
+                showChartData();
+                invalidateOptionsMenu();
+                return true;
+            case R.id.menu_stop:
+                run = false;
+                invalidateOptionsMenu();
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // Поток для непрерывного отображения данных
+    private void showChartData() {
+        thr = new Thread(){
+            public void run() {
+                while (run) {
+                    try {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                addChartData();
+                            }
+                        });
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        thr.start();
+    }
+
+    byte[] d = new byte[20];
+    private void addChartData() {
+
+
+            hr_chart.drawChart(null, (int) (Math.random()*100));
+
+            for(int y = 0; y<20; y++){
+                d[y] = (byte)(Math.random()*255);
+            }
+            ecg_chart.drawChart(d,0);
+
     }
 }
